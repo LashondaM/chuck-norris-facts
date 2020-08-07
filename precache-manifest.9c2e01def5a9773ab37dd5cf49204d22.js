15c11437ce576b7a944686fc7979e13e0c415222
Lashonda Miller <lashondamiller007@gmail.com>
2020-08-07 14:16:28 -0400

    Updates

---
+++ precache-manifest.9c2e01def5a9773ab37dd5cf49204d22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774080057168316121fa02712f447533",
+    "url": "/chuck-norris-facts/index.html"
+  },
+  {
+    "revision": "f18730fe3c55c48c88a3",
+    "url": "/chuck-norris-facts/static/css/main.e7245a9c.chunk.css"
+  },
+  {
+    "revision": "4d62ddfc27cd3a2471dd",
+    "url": "/chuck-norris-facts/static/js/2.b63796cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck-norris-facts/static/js/2.b63796cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18730fe3c55c48c88a3",
+    "url": "/chuck-norris-facts/static/js/main.c8f95fa1.chunk.js"
+  },
+  {
+    "revision": "9afe84aeec9d7698f7c1",
+    "url": "/chuck-norris-facts/static/js/runtime-main.bc87c0ad.js"
+  },
+  {
+    "revision": "1ad0f5b71ef5f2581bc30ef60823807c",
+    "url": "/chuck-norris-facts/static/media/chuckMan.1ad0f5b7.jpg"
+  }
+]);
